363c1460c2d27b4ecc265b236c667990e12782fa
x0xBecka26x0x <156142455+x0xBecka26x0x@users.noreply.github.com>
2024-12-02 17:40:31 -0500

    trying to fix the forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -4,38 +4,48 @@ import axios from "axios";
 import WeatherForecastDay from "./WeatherForecastDay";
 
 export default function WeatherForecast(props) {
-  let [loaded, setLoaded] = useState(false);
+  let [loaded, setLoaded] = useState([]);
   let [forecast, setForecast] = useState(null);
 
   function handleResponse(response) {
+    console.log(response);
     setForecast(response.data.daily);
     setLoaded(true);
   }
 
-  if (loaded) {
-    return (
-      <div className="WeatherForecast">
-        <div className="row">
-            <div className="col">
-                <WeatherForecastDay data={forecast} />
-            </div>
-        </div>
-    </div>
-    );
-} else {
+  function load() {
     let apiKey = "bf8f1010b3c486eaa378at4e5eo24f84";
     let city = props.data.city;
     let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
 
     axios.get(apiUrl).then(handleResponse);
 
-    return null;
+  }
+
+
+  if (loaded) {
+    console.log(forecast);
+    if (forecast && Array.isArray(forecast)) {
+      return (
+          <div className="WeatherForecast">
+              <div className="row">
+                  {forecast.map(function (dailyForecast, index) {
+                      if (index < 6) {
+                          return (
+                              <div className="col" key={index}>
+                                  <WeatherForecastDay data={dailyForecast} />
+                              </div>
+                          );
+                      } else {
+                          return null;
+                      }
+                  })}
+              </div>
+          </div>
+      );
+  } else {
+      load();
+      return null;
+  }
   }
 }
-
-   
-
-
-        //console.log(response);
-        //bf8f1010b3c486eaa378at4e5eo24f84
-        //https://api.shecodes.io/weather/v1/forecast?query=Lisbon&key=bf8f1010b3c486eaa378at4e5eo24f84&units=metric//
